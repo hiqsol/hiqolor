34d42983d2f2fc898e90c37e9a0f91821b817947
Andrii Vasyliev <sol@hiqdev.com>
2020-12-08 12:45:12 +0200

    Add `convert.sh`

---
+++ schemes/convert.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+../bin/sc bright.Xresource  bright.colorscheme
+../bin/sc bright.Xresource  bright.colortable
+../bin/sc bright.Xresource  bright.css
+../bin/sc left.colortable   left.colorscheme
+../bin/sc left.colortable   left.css
+../bin/sc normal.colortable normal.colorscheme
+../bin/sc normal.colortable normal.css
+../bin/sc one.colortable    one.colorscheme
+../bin/sc one.colortable    one.css
+../bin/sc right.colortable  right.colorscheme
+../bin/sc right.colortable  right.css
+../bin/sc two.colortable    two.colorscheme
+../bin/sc two.colortable    two.css
