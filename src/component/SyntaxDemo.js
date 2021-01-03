18447f0d1aa7851558b287a93a4e4a6877f56439
Andrii Vasyliev <sol@hiqdev.com>
2021-01-03 14:03:12 +0000

    Tune syntax demo style

--- src/component/SyntaxDemo.js
+++ src/component/SyntaxDemo.js
@@ -7,131 +7,123 @@ const SyntaxDemo = observer(() => {
 
   return (
     <div id="syntax-demo">
-      <span className="Comment">
-        Comment	any comment
-      </span><br/>
-      <br/>
+      <table border="0">
+        <tr className="Comment">
+          <td>*</td><td>Comment</td><td>any comment</td>
+        </tr>
 
-      <span className="Constant">
-        Constant	any constant
-      </span><br/>
-      <span className="String">
-         String		a string constant: "this is a string"
-      </span><br/>
-      <span className="Character">
-         Character	a character constant: 'c', '\n'
-      </span><br/>
-      <span className="Number">
-         Number		a number constant: 234, 0xff
-      </span><br/>
-      <span className="Boolean">
-         Boolean	a boolean constant: TRUE, false
-      </span><br/>
-      <span className="Float">
-         Float		a floating point constant: 2.3e10
-      </span><br/>
-      <br/>
+        <tr className="Constant next-item">
+          <td>*</td><td>Constant</td><td>any constant</td>
+        </tr>
+        <tr className="String">
+           <td></td><td>String</td><td>a string constant: "this is a string"</td>
+        </tr>
+        <tr className="Character">
+           <td></td><td>Character</td><td>a character constant: 'c', '\n'</td>
+        </tr>
+        <tr className="Number">
+           <td></td><td>Number</td><td>a number constant: 234, 0xff</td>
+        </tr>
+        <tr className="Boolean">
+           <td></td><td>Boolean</td><td>a boolean constant: TRUE, false</td>
+        </tr>
+        <tr className="Float">
+           <td></td><td>Float</td><td>a floating point constant: 2.3e10</td>
+        </tr>
 
-      <span className="Identifier">
-	      Identifier	any variable name
-      </span><br/>
-      <span className="Function">
-	      Function	function name (also: methods for classes)
-      </span><br/>
-      <br/>
+        <tr className="Identifier next-item">
+          <td>*</td><td>Identifier</td><td>any variable name</td>
+        </tr>
+        <tr className="Function">
+          <td></td><td>Function</td><td>function name (also: methods for classes)</td>
+        </tr>
 
-      <span className="Statement">
-	      Statement	any statement
-      </span><br/>
-      <span className="Conditional">
-	      Conditional	if, then, else, endif, switch, etc.
-      </span><br/>
-      <span className="Repeat">
-	      Repeat		for, do, while, etc.
-      </span><br/>
-      <span className="Label">
-	      Label		case, default, etc.
-      </span><br/>
-      <span className="Operator">
-	      Operator	"sizeof", "+", "*", etc.
-      </span><br/>
-      <span className="Keyword">
-	      Keyword	any other keyword
-      </span><br/>
-      <span className="Exception">
-	      Exception	try, catch, throw
-      </span><br/>
-      <br/>
+        <tr className="Statement next-item">
+          <td>*</td><td>Statement</td><td>any statement</td>
+        </tr>
+        <tr className="Conditional">
+          <td></td><td>Conditional</td><td>if, then, else, endif, switch, etc.</td>
+        </tr>
+        <tr className="Repeat">
+          <td></td><td>Repeat</td><td>for, do, while, etc.</td>
+        </tr>
+        <tr className="Label">
+          <td></td><td>Label</td><td>case, default, etc.</td>
+        </tr>
+        <tr className="Operator">
+          <td></td><td>Operator</td><td>"sizeof", "+", "*", etc.</td>
+        </tr>
+        <tr className="Keyword">
+          <td></td><td>Keyword</td><td>any other keyword</td>
+        </tr>
+        <tr className="Exception">
+          <td></td><td>Exception</td><td>try, catch, throw</td>
+        </tr>
 
-      <span className="PreProc">
-	      PreProc	generic Preprocessor
-      </span><br/>
-      <span className="Include">
-	      Include	preprocessor #include
-      </span><br/>
-      <span className="Define">
-	      Define		preprocessor #define
-      </span><br/>
-      <span className="Macro">
-	      Macro		same as Define
-      </span><br/>
-      <span className="PreCondit">
-	      PreCondit	preprocessor #if, #else, #endif, etc.
-      </span><br/>
-      <br/>
+        <tr className="PreProc next-item">
+          <td>*</td><td>PreProc</td><td>generic Preprocessor</td>
+        </tr>
+        <tr className="Include">
+          <td></td><td>Include</td><td>preprocessor #include</td>
+        </tr>
+        <tr className="Define">
+          <td></td><td>Define</td><td>preprocessor #define</td>
+        </tr>
+        <tr className="Macro">
+          <td></td><td>Macro</td><td>same as Define</td>
+        </tr>
+        <tr className="PreCondit">
+          <td></td><td>PreCondit</td><td>preprocessor #if, #else, #endif, etc.</td>
+        </tr>
 
-      <span className="Type">
-	      Type		int, long, char, etc.
-      </span><br/>
-      <span className="StorageClass">
-	      StorageClass	static, register, volatile, etc.
-      </span><br/>
-      <span className="Structure">
-	      Structure	struct, union, enum, etc.
-      </span><br/>
-      <span className="Typedef">
-	      Typedef	A typedef
-      </span><br/>
-      <br/>
+        <tr className="Type next-item">
+          <td>*</td><td>Type</td><td>int, long, char, etc.</td>
+        </tr>
+        <tr className="StorageClass">
+          <td></td><td>StorageClass</td><td>static, register, volatile, etc.</td>
+        </tr>
+        <tr className="Structure">
+          <td></td><td>Structure</td><td>struct, union, enum, etc.</td>
+        </tr>
+        <tr className="Typedef">
+          <td></td><td>Typedef</td><td>A typedef</td>
+        </tr>
 
-      <span className="Special">
-	      Special	any special symbol
-      </span><br/>
-      <span className="SpecialChar">
-	      SpecialChar	special character in a constant
-      </span><br/>
-      <span className="Tag">
-	      Tag		you can use CTRL-] on this
-      </span><br/>
-      <span className="Delimiter">
-	      Delimiter	character that needs attention
-      </span><br/>
-      <span className="SpecialComment">
-	      SpecialComment	special things inside a comment
-      </span><br/>
-      <span className="Debug">
-	      Debug		debugging statements
-      </span><br/>
-      <br/>
+        <tr className="Special next-item">
+          <td>*</td><td>Special</td><td>any special symbol</td>
+        </tr>
+        <tr className="SpecialChar">
+          <td></td><td>SpecialChar</td><td>special character in a constant</td>
+        </tr>
+        <tr className="Tag">
+          <td></td><td>Tag</td><td>you can use CTRL-] on this</td>
+        </tr>
+        <tr className="Delimiter">
+          <td></td><td>Delimiter</td><td>character that needs attention</td>
+        </tr>
+        <tr className="SpecialComment">
+          <td></td><td>SpecialComment&nbsp;&nbsp;</td><td>special things inside a comment</td>
+        </tr>
+        <tr className="Debug">
+          <td></td><td>Debug</td><td>debugging statements</td>
+        </tr>
 
-      <span className="Underlined">
-	      Underlined	text that stands out, HTML links
-      </span><br/>
-      <br/>
+        <tr className="Underlined next-item">
+          <td>*</td><td>Underlined</td><td>text that stands out, HTML links</td>
+        </tr>
 
-      <span className="Ignore">
-	      Ignore		left blank, hidden  |hl-Ignore|
-      </span><br/>
-      <br/>
+        <tr className="Ignore next-item">
+          <td>*</td><td>Ignore</td><td>left blank, hidden</td>
+        </tr>
 
-      <span className="Error">
-	      Error		any erroneous construct
-      </span><br/>
-      <br/>
+        <tr className="Error">
+          <td>*</td><td>Error</td><td>any erroneous construct</td>
+        </tr>
 
-      <span className="Todo">
-	      Todo		anything that needs extra attention; mostly the
-      </span><br/>
+        <tr className="Todo">
+          <td>*</td><td>Todo</td><td>anything that needs extra attention; mostly the keywords TODO FIXME and XXX</td>
+        </tr>
+      </table>
     </div>
   );
 })
